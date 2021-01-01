54ec9a4faeb8576e37edfe8d97771f95fd3bbc59
KostaSav <ksavvidis@outlook.com>
2021-01-01 21:38:57 +0200

    first commit

--- 2020/day7.py
+++ 2020/day7.py
@@ -3,13 +3,11 @@ with open('2020\day7.txt', 'r') as f:
 
 # print(rules)
 
-container = []
-contents = []
+#################### Part One ####################
 rules_dict = dict()
 
 for rule in rules:
     # print(rule)
-    # container.append(rule.split("contain")[0])
     keyBag = rule.split("contain")[0].replace("bags", "").strip()
     # print(keyBag + ":")
     contents_str = rule.split("contain")[1]
@@ -54,4 +52,43 @@ def count_bags(input_bag):
 
 
 print(count_bags("shiny gold"))
+#################### Part One ####################
 
+
+#################### Part Two ####################
+rules_dict_2 = dict()
+
+for rule in rules:
+    # print(rule)
+    keyBag = rule.split("contain")[0].replace("bags", "").strip()
+    # print(keyBag + ":")
+    contents_str = rule.split("contain")[1]
+    contents_arr = contents_str.split(",")
+    for i in range(len(contents_arr)):
+        contents_arr[i] = contents_arr[i].rstrip(
+            ".").replace("bags", "").replace("bag", "").strip()
+
+    rules_dict_2.update({keyBag: contents_arr})
+
+# for key, value in rules_dict_2.items():
+#     print(key, value)
+
+total = -1
+
+bags = {"shiny gold": 1}
+
+while len(bags) > 0:
+    # print(bags)
+    # print()
+    key = list(bags.keys())[0]
+    total += bags[key]
+    if key in rules_dict_2 and "no other" not in rules_dict_2[key]:
+        for newbag in rules_dict_2[key]:
+            newbag_name = newbag.split(" ")[1]+" "+newbag.split(" ")[2]
+            if newbag_name not in bags:
+                bags[newbag_name] = 0
+            bags[newbag_name] += (list(bags.values())[0] * int(newbag[0]))
+    del bags[key]
+
+print(total)
+#################### Part Two ####################
