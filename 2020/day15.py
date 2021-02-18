b6ae8b036aa06536ab9a0c0a43901b22f36e2e4d
KostaSav <ksavvidis@outlook.com>
2021-02-18 12:12:48 +0200

    Added Part Two

--- 2020/day15.py
+++ 2020/day15.py
@@ -1,9 +1,9 @@
 with open("2020\day15.txt", "r") as f:
-    starting = f.read().replace("\n", "")
+    numbers = f.read().replace("\n", "")
 
 #################### Part One ####################
 # Convert our input to an array of integers
-starting = [int(x) for x in starting.split(",")]
+starting = [int(x) for x in numbers.split(",")]
 count = len(starting)
 
 while count < 2020:
@@ -23,3 +23,34 @@ while count < 2020:
 
 print(starting[2019])
 #################### Part One ####################
+
+
+#################### Part Two ####################
+# Probably not the best solution, given the time it takes to complete...
+# Used a dictionary to store each value as a key only once and their last position as a value
+positions = dict()
+
+initial = [int(x) for x in numbers.split(",")]
+last_number = initial[len(initial) - 1]
+count = 1
+
+for number in initial[:-1]:
+    positions.update({number: count})
+    count += 1
+
+
+while count <= 30000000:
+    if last_number in positions.keys():
+        temp = last_number
+        last_number = count - positions[last_number]
+        positions.update({temp: count})
+        if count == 30000000:
+            print(temp)
+        count += 1
+    else:
+        positions.update({last_number: count})
+        if count == 30000000:
+            print(temp)
+        count += 1
+        last_number = 0
+#################### Part Two ####################
