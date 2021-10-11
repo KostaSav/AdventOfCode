999de58cca4d75217866a45de3f5bf36cc53833a
KostaSav <ksavvidis@outlook.com>
2021-10-11 19:56:29 +0300

    Add day 2
    https://adventofcode.com/2015/day/2

---
+++ 2015/day02.py
@@ -0,0 +1,43 @@
+with open("2015\day02.txt", "r") as f:
+    entries_arr = [line.strip() for line in f]
+
+# ------ Part One ------ #
+total = 0
+
+for entry in entries_arr:
+    l = int(entry.split("x")[0])
+    w = int(entry.split("x")[1])
+    h = int(entry.split("x")[2])
+    surface_1 = l * w
+    surface_2 = w * h
+    surface_3 = h * l
+    small_side = min(surface_1, surface_2, surface_3)
+    paper = 2 * surface_1 + 2 * surface_2 + 2 * surface_3 + small_side
+    total += paper
+
+print(total)
+
+# ------ End of Part One ------ #
+
+
+# ------ Part Two ------ #
+total_ribbon = 0
+
+for entry in entries_arr:
+    l = int(entry.split("x")[0])
+    w = int(entry.split("x")[1])
+    h = int(entry.split("x")[2])
+    sides = [l, w, h]
+
+    smallest_first = min(sides)
+    sides.remove(smallest_first)
+    smallest_second = min(sides)
+
+    bow = l * w * h
+
+    ribbon = 2 * smallest_first + 2 * smallest_second + bow
+    total_ribbon += ribbon
+
+print(total_ribbon)
+
+# ------ End of Part Two ------ #
